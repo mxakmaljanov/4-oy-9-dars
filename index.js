8a2a43a6be9d25036a57610460bc1683cec754a6
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-06-07 20:38:16 +0500

    4-oy 9-dars

---
+++ index.js
@@ -0,0 +1,90 @@
+//1 - masala
+//let arr = [];
+//for (let i = 1; i <= 5; i++) {
+//  arr.push(1);
+//  console.log(arr);
+//}
+//
+//2-masala
+//let arr = [];
+//arr.push(1,2,3);
+//arr.pop();
+//arr.toString;
+//console.log(arr);
+//
+//3-masala
+//let arr = [];
+//arr.unshift(1,2,3,4);
+//arr.reverse();
+//console.log(arr,'uzunlik->',arr.length);
+//
+//4-masala
+//let arr = [];
+//arr.unshift(1,2,3);
+//arr.shift(0);
+//arr.join()
+//console.log(arr);
+//
+//5-masala
+//
+//let arr = [3,true, 4, 'true'];
+//arr.pop();
+//console.log('uzunluk->',arr.length, 'massiv->', arr, 'qiymat->',arr.at(0));
+//
+//6-masala
+//let arr = [1,false,'true',4];
+//arr.push('sss');
+//arr.toString()
+//console.log(typeof arr.at(-1));
+//
+//7-masala
+//let arr = [1,2,3,4]
+//arr.unshift(11);
+//arr.reverse();
+//console.log(arr, arr.length);
+//
+//8-masala
+//let arr = [1,2,3,4];
+//arr.shift(0);
+//let str1 = arr.join()
+//console.log(str1, arr.length);
+//
+//9-masala
+//let arr = [1,2,3,4];
+//arr.push(9,32);
+//arr.unshift(12);
+//let uzunlik = arr.length;
+//let teskari = arr.reverse();
+//let string = arr.join();
+//console.log(teskari, string, uzunlik);
+//
+//10-masala
+//let arr = [1,2,3,4,5,6]
+//let str1 = arr.pop();
+//let str2 = arr.shift();
+//let uzunligi = arr.length;
+//let teskari = arr.reverse();
+//console.log(teskari, uzunligi);
+//
+//11-masala
+//let arr = [1,2,3,4,5]
+//let teskari = arr.reverse();
+//let string = arr.toString();
+//let uzunligi = string.length;
+//console.log(teskari, string , uzunligi);
+//
+//12-masala
+//
+// let arr = [1,2,3,4,5]
+// let arr1 = [6,7,8,9]
+// let qoshildi = arr.concat(arr1);
+// let olindi = qoshildi.slice(1,-1);
+// let uzunligi = qoshildi.length;
+// console.log(qoshildi, uzunligi);
+//
+//13-masala
+//let arr =[1,2,3,4,5,6,7,8,9];
+//let ochirildi = arr.splice(2 , 3);
+//let string = ochirildi.toString();
+//let uzunligi = string.length;
+//console.log(ochirildi , string , uzunligi);
